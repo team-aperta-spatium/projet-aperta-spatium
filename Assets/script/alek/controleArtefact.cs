07835877ae8f4f71d215faba3174520fa7d42f94
Alek Crépeau <acrepeau@3saisons.ca>
2024-04-22 00:42:14 -0400

    ajustement pour la version finale du sprint 2

---
+++ Assets/script/alek/controleArtefact.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class controleArtefact : MonoBehaviour
+{
+    public Transform perso;
+    public float distanceDetection;
+    public GameObject txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceAuPerso = Vector3.Distance(transform.position, perso.position);
+
+        if (distanceAuPerso <= distanceDetection)
+        {
+            txt.SetActive(true);
+
+            Debug.Log("true");
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                SceneManager.LoadScene("boss");
+            }
+        }
+        else
+        {
+            txt.SetActive(false);
+            Debug.Log("false");
+        }
+    }
+}
--- Assets/script/alek/duplicateur.cs
+++ Assets/script/alek/duplicateur.cs
@@ -2,7 +2,6 @@ using BrewedInk.CRT;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.UI;
 
