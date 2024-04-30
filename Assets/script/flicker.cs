a471fb31ecb01e3f3cfa5854c051351f6716d718
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-04-30 16:42:32 -0400

    ajout script flicker
    nouveau scripte flicker pour les lumiere:
    var Intensite: l'intensite de base de la lumiere
    var Raritee: plus le chiffre est gros, moins la lumiere va flicker (100 baseline)

---
+++ Assets/script/flicker.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class flicker : MonoBehaviour
+{
+    public float intensite;
+    public int raritee;
+    bool allumee = true;
+    void Update()
+    {
+        int randomNb = Random.Range(1, raritee);
+        if(randomNb == 99)
+        {
+            fermer();
+            Invoke("gererFlick", 0.1f);
+        }
+    }
+
+    void gererFlick()
+    {
+        if(intensite <= 20)
+        {
+            allumer();
+            Invoke("fermer", 0.3f); 
+        }
+        else
+        {
+            allumer();
+        }
+    }
+
+    void allumer()
+    {
+        gameObject.GetComponent<Light>().intensity = intensite;
+    }
+
+    void fermer()
+    {
+        gameObject.GetComponent<Light>().intensity = 0;
+    }
+}
