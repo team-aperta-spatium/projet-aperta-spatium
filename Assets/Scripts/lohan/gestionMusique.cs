b3e241106178687322884e629044887aa12cec09
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-06-03 01:24:40 -0400

    amelioration de la gestion sonore

--- Assets/Scripts/lohan/gestionMusique.cs
+++ Assets/Scripts/lohan/gestionMusique.cs
@@ -9,10 +9,18 @@ public class gestionMusique : MonoBehaviour
     [SerializeField] AudioSource sourceMusique;
     [SerializeField] AudioSource sons;
     [SerializeField] AudioSource bruitPas;
+    [SerializeField] AudioSource sourceNarrateur;
+    [SerializeField] AudioSource coupEpee;
     [Header("AudioClip")]
     public AudioClip musique;
     public AudioClip ok;
     public AudioClip footstep;
+    public AudioClip narrateur;
+    public AudioClip coupEpeeFaible1;
+    public AudioClip coupEpeeFaible2;
+    public AudioClip coupEpeeFaible3;
+    public AudioClip coupEpeeFort1;
+    public AudioClip coupEpeeFort2;
     [Header("valeurs autres")]
     public GameObject perso;
     public float multiPitch;
@@ -36,6 +44,10 @@ public class gestionMusique : MonoBehaviour
         {
             perso = GameObject.Find("/persoParent/perso");
             scene = SceneManager.GetActiveScene();
+            if(scene.name == "tutoriel")
+            {
+                jouerNarrateur();
+            }
         }
 
         if (perso != null && perso.GetComponent<Rigidbody>().velocity != new Vector3(0,0,0) && (perso.GetComponent<mouvement2>().etat == mouvement2.etatMouve.marche || perso.GetComponent<mouvement2>().etat == mouvement2.etatMouve.course))
@@ -58,10 +70,62 @@ public class gestionMusique : MonoBehaviour
         {
             bruitPas.Stop();
         }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            jouerEpeeFaible();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            jouerEpeeFort();
+        }
     }
 
     public void jouerOk()
     {
         sons.PlayOneShot(ok);
+    }
+
+    public void jouerNarrateur()
+    {
+        sourceNarrateur.PlayOneShot(narrateur);
+        sourceMusique.volume = 0.15f;
+        float length = narrateur.length;
+        Invoke("resetSons", length);
+    }
+
+    public void resetSons()
+    {
+        sourceMusique.volume = 0.5f;
+    }
+
+    public void jouerEpeeFaible()
+    {
+        float valAle = Random.Range(1, 4);
+        if(valAle == 1)
+        {
+            coupEpee.PlayOneShot(coupEpeeFaible1);
+        }
+        else if(valAle == 2)
+        {
+            coupEpee.PlayOneShot(coupEpeeFaible2);
+        }
+        else if (valAle == 3)
+        {
+            coupEpee.PlayOneShot(coupEpeeFaible3);
+        }
+    }
+
+    public void jouerEpeeFort()
+    {
+        float valAle = Random.Range(1, 3);
+        if (valAle == 1)
+        {
+            coupEpee.PlayOneShot(coupEpeeFort1);
+        }
+        else if (valAle == 2)
+        {
+            coupEpee.PlayOneShot(coupEpeeFort2);
+        }
     }
 }
